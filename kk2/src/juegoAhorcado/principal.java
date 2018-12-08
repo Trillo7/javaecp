c038182764aac7e5a7e9cb8c51f1dbba8336099d
Francisco pc <trenderx@gmail.com>
2018-12-08 03:01:05 +0100

    subete

--- kk2/src/juegoAhorcado/principal.java
+++ kk2/src/juegoAhorcado/principal.java
@@ -4,12 +4,12 @@ public class principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		baseMuneco.getJuego().cargaJuego();
-		baseMuneco.getJuego().iniciaJugada();
 		// dudas: Como cambiar el array literal? en vez de manualmente
 		// Como no hacer un metodo para cada imagen? Al utilizar el getImagen y mandarle el nombre del archivo de imagen como que se refrescaba mas? flashea
 		// y tmb si en el getahorcadofondo cambiando las variables tmb flashea
+		baseMuneco.getJuego().cargaJuego();
+		baseMuneco.getJuego().iniciaJugada();
+		
 	}
 
 }
