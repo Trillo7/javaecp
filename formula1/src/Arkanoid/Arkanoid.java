3ee54474a5c9025f900ebeea29ad75ae17d66f0e
diurno <diurno@pc-aula>
2019-01-25 14:59:33 +0100

    juego arkanoid

---
+++ formula1/src/Arkanoid/Arkanoid.java
@@ -0,0 +1,69 @@
+package Arkanoid;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import tutorialJava.SpaceInvaders.version17.Player;
+import tutorialJava.SpaceInvaders.version17.SpriteCache;
+
+public class Arkanoid extends Canvas implements KeyListener {
+	private BufferStrategy strategy;
+	private long usedTime;
+	
+	private SpriteCache spriteCache;
+	private ArrayList actors; 
+
+	public Arkanoid() {
+		spriteCache = new SpriteCache();
+		
+		
+		JFrame ventana = new JFrame("Invaders");
+		JPanel panel = (JPanel)ventana.getContentPane();
+		setBounds(0,0,Stage.WIDTH,Stage.HEIGHT);
+		panel.setPreferredSize(new Dimension(Stage.WIDTH,Stage.HEIGHT));
+		panel.setLayout(null);
+		panel.add(this);
+		ventana.setBounds(0,0,Stage.WIDTH,Stage.HEIGHT);
+		ventana.setVisible(true);
+		ventana.addWindowListener( new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		ventana.setResizable(false);
+		createBufferStrategy(2);
+		strategy = getBufferStrategy();
+		requestFocus();
+		addKeyListener(this);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
