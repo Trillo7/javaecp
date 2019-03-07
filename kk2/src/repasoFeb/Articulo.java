0dd86a321fc1f97aed7e4b275c6b73b3085d81f4
Trillo diurno <fr.trillo7@gmail.com>
2019-03-07 13:58:03 +0100

    hashmap

---
+++ kk2/src/repasoFeb/Articulo.java
@@ -0,0 +1,21 @@
+package repasoFeb;
+
+import java.util.List;
+
+public class Articulo  {
+	public String codigo;
+	public String estante;
+	public String descripcion;
+	/**
+	 * @param codigo
+	 * @param estante
+	 * @param descripcion
+	 */
+	public Articulo(String codigo, String estante, String descripcion) {
+		super();
+		this.codigo = codigo;
+		this.estante = estante;
+		this.descripcion = descripcion;
+	}
+	
+}
---
+++ kk2/src/repasoFeb/hashmapej1.java
@@ -0,0 +1,108 @@
+package repasoFeb;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class hashmapej1 {
+	private HashMap<String, Articulo> almacenmap = new HashMap<String, Articulo>();
+	public String articuloslist="";
+
+	
+	public hashmapej1() {
+		JFrame f=new JFrame("Aplicacion almacen");  
+	    JButton b=new JButton("Añadir articulo");  
+	    b.setBounds(100,80,195,30); 
+	    f.add(b);
+	    JButton c=new JButton("Borrar articulo");
+	    c.setBounds(100,120,195,30); 
+	    f.add(c);  
+	    JButton d=new JButton("Actualizar estante");
+	    d.setBounds(100,160,195,30); 
+	    f.add(d); 
+	    JButton e=new JButton("Mostrar articulos");
+	    e.setBounds(100,200,195,30); 
+	    f.add(e); 
+	    f.setSize(400,400);  
+	    f.setLayout(null);  
+	    f.setVisible(true);   
+	    b.addActionListener(new PageActionListener(1));
+	    c.addActionListener(new PageActionListener(2));
+	    d.addActionListener(new PageActionListener(3));
+	    e.addActionListener(new PageActionListener(4));
+
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hashmapej1 main=new hashmapej1();
+	}
+	//FUNCIONES BOTONES
+	public void anadirarticulo() {
+		System.out.println("Añadir articulo");
+		String codigo=JOptionPane.showInputDialog("Codigo de barras: ");
+		String estante=JOptionPane.showInputDialog("Introduce el estante: ");
+		String descripcion=JOptionPane.showInputDialog("Introduce una breve descripcion: ");
+		Articulo articulo=new Articulo(codigo,estante,descripcion);
+		almacenmap.put(articulo.codigo, articulo);
+	}
+	
+	public void borrararticulo() {
+		System.err.println("Borrar articulo");
+	}
+	public void actualizaEstante() {
+		System.err.println("Actualizamos estante");
+
+	}
+	public void muestraArticulos() {
+		System.err.println("Mostramos articulos");
+		System.out.println("\nRecorrido de los objetos del HashMap:");
+		articuloslist="";
+		Iterator<Articulo> personas = almacenmap.values().iterator();
+		if(personas.hasNext()) {
+			while (personas.hasNext()) {
+				Articulo articulo=personas.next();
+				articuloslist+=articulo.codigo+" - "+articulo.descripcion+"\n";
+			}
+			JOptionPane.showMessageDialog(null, articuloslist);
+		}
+
+
+	}
+	private class PageActionListener implements ActionListener {
+	    private int opcion;
+
+	    public PageActionListener(int opcion) {
+	        this.opcion = opcion;
+	    }
+
+	    public void actionPerformed(ActionEvent e) {
+	        if(opcion==1) {
+	        	anadirarticulo();
+	        }
+	        if(opcion==2) {
+	        	borrararticulo();
+	        }
+	        if(opcion==3) {
+	        	actualizaEstante();
+	        }
+	        if(opcion==4) {
+	        	muestraArticulos();
+	        }
+	    }
+
+	}
+}
